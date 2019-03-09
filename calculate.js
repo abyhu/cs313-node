b047239fc1edece010372d90bd81c62ef03de112
amarro <abyhunter@gmail.com>
2019-03-09 07:38:41 -0700

    Added correct pricing logic

--- calculate.js
+++ calculate.js
@@ -17,19 +17,14 @@ function getPrice(response, pounds, ounces, type) {
 		default:
 			if (pounds == 0) {
 				var zeroPoundParcelPrices = [0.00, 3.66, 4.39, 5.19, 5.71];
-				if (ounces == 0)
-					price= zeroPoundParcelPrices[0];
-				else if (ounces >= 1 && ounces < 5)
-					price = zeroPoundParcelPrices[1];
-				else if (ounces >=5 && ounces < 9 )
-					price = zeroPoundParcelPrices[2];
-				else if (ounces >= 9 && ounces < 13)
-					price = zeroPoundParcelPrices[3];
-				else 
-					price = zeroPoundParcelPrices[4];
+				if (ounces == 0) { price= zeroPoundParcelPrices[0]; }
+				else if (ounces >= 1 && ounces < 5) { price = zeroPoundParcelPrices[1]; }
+				else if (ounces >=5 && ounces < 9 ) { price = zeroPoundParcelPrices[2]; }
+				else if (ounces >= 9 && ounces < 13) { price = zeroPoundParcelPrices[3]; }
+				else { price = zeroPoundParcelPrices[4]; }
 			} else {
 				var poundParcelPrices = [5.71, 6.68, 7.85, 8.30, 8.75, 9.80, 10.55, 11.50, 11.85, 12.30, 13.10, 13.95, 15.20, 16.10, 17.10, 17.80, 18.30, 19.15,19.50, 20.05, 20.90, 21.60, 22.10, 22.60, 23.15, 23.35, 24.30, 25.05, 25.80, 26.60, 27.40, 28.20, 28.50, 29.00, 29.25, 29.55, 29.85, 30.15, 30.50, 30.80, 31.15];
-				price = [pounds];
+				price = poundParcelPrices[pounds];
 			}
 			break;
 		}
